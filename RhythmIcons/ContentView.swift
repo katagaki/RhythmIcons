9eaaeba09107bd4bf45dd9aebaa3170c6f07ede6
katagaki <68231035+katagaki@users.noreply.github.com>
2024-05-18 15:16:55 +0900

    Add initial icons

--- RhythmIcons/ContentView.swift
+++ RhythmIcons/ContentView.swift
@@ -5,20 +5,77 @@
 //  Created by シン・ジャスティン on 2024/05/18.
 //
 
+import Komponents
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            Section {
+                ListRow(image: "Chunithm.1", title: "ロゴマーク付き")
+                ListRow(image: "Chunithm.2", title: "ロゴマークなし")
+                ListRow(image: "Chunithm.3", title: "コントローラーサイズアップ（ロゴマーク付き）")
+                ListRow(image: "Chunithm.4", title: "コントローラーサイズアップ（ロゴマークなし）")
+                ListRow(image: "Chunithm.5", title: "シンプルコントローラー（ロゴマーク付き）")
+                ListRow(image: "Chunithm.6", title: "シンプルコントローラー（ロゴマークなし）")
+                ListRow(image: "Chunithm.7", title: "ロゴマークのみ")
+            } header: {
+                Text(verbatim: "チュウニズム")
+                    .textCase(.none)
+            }
+            Section {
+                ListRow(image: "IIDX.1", title: "クラシック")
+                ListRow(image: "IIDX.2", title: "シンプル１")
+                ListRow(image: "IIDX.3", title: "シンプル２")
+                ListRow(image: "IIDX.4", title: "EPOLIS")
+            } header: {
+                Text(verbatim: "beatmaniaIIDX")
+                    .textCase(.none)
+            }
+            Section {
+                ListRow(image: "Maimai.1", title: "でらっくま１")
+                ListRow(image: "Maimai.2", title: "でらっくま２")
+                ListRow(image: "Maimai.3", title: "でらっくま３")
+                ListRow(image: "Maimai.4", title: "でらっくま４")
+            } header: {
+                Text(verbatim: "maimai")
+                    .textCase(.none)
+            }
+            Section {
+                ListRow(image: "Ongeki.1", title: "カラー付き")
+                ListRow(image: "Ongeki.2", title: "カラーなし")
+                ListRow(image: "Ongeki.3", title: "ロゴマークのみ")
+            } header: {
+                Text(verbatim: "オンゲキ")
+                    .textCase(.none)
+            }
+            Section {
+                ListRow(image: "SDVX.1", title: "ロゴマーク付き")
+                ListRow(image: "SDVX.2", title: "ロゴマークなし")
+                ListRow(image: "SDVX.3", title: "ロゴカラー１")
+                ListRow(image: "SDVX.4", title: "ロゴカラー２")
+                ListRow(image: "SDVX.5", title: "ロゴカラー３")
+                ListRow(image: "SDVX.6", title: "ロゴマークのみ")
+            } header: {
+                Text(verbatim: "SOUND VOLTEX")
+                    .textCase(.none)
+            }
+            Section {
+                ListRow(image: "Taiko.1", title: "カラー１")
+                ListRow(image: "Taiko.2", title: "カラー２")
+            } header: {
+                Text(verbatim: "太鼓の達人")
+                    .textCase(.none)
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    Group {
+        ContentView()
+            .environment(\.colorScheme, .light)
+        ContentView()
+            .environment(\.colorScheme, .dark)
+    }
 }
