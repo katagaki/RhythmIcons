ab0ca0f53d530c35686991a9674e22afd3bb069a
katagaki <68231035+katagaki@users.noreply.github.com>
2024-05-18 22:41:14 +0900

    Add navigation titles

--- RhythmIcons/Tab Views/TemplateIconsView.swift
+++ RhythmIcons/Tab Views/TemplateIconsView.swift
@@ -9,63 +9,66 @@ import SwiftUI
 
 struct TemplateIconsView: View {
     var body: some View {
-        List {
-            Section {
-                HStack {
-                    Group {
-                        Image("Chunithm.Glyph.1")
-                            .resizable()
-                        Image("Maimai.Glyph.1")
-                            .resizable()
-                        Image("Ongeki.Glyph.1")
-                            .resizable()
-                        Image("Taiko.Glyph.1")
-                            .resizable()
-                        Image("IIDX.Glyph.1")
-                            .resizable()
-                        Image("SDVX.Glyph.1")
-                            .resizable()
-                        Image("Popn.Glyph.1")
-                            .resizable()
-                        Image("Polaris.Glyph.1")
-                            .resizable()
+        NavigationStack {
+            List {
+                Section {
+                    HStack {
+                        Group {
+                            Image("Chunithm.Glyph.1")
+                                .resizable()
+                            Image("Maimai.Glyph.1")
+                                .resizable()
+                            Image("Ongeki.Glyph.1")
+                                .resizable()
+                            Image("Taiko.Glyph.1")
+                                .resizable()
+                            Image("IIDX.Glyph.1")
+                                .resizable()
+                            Image("SDVX.Glyph.1")
+                                .resizable()
+                            Image("Popn.Glyph.1")
+                                .resizable()
+                            Image("Polaris.Glyph.1")
+                                .resizable()
+                        }
+                        .scaledToFit()
+                        .frame(height: 28.0)
+                        .frame(maxWidth: .infinity)
                     }
-                    .scaledToFit()
-                    .frame(height: 28.0)
-                    .frame(maxWidth: .infinity)
+                } header: {
+                    Text(verbatim: "Glyphs")
+                        .textCase(.none)
                 }
-            } header: {
-                Text(verbatim: "Glyphs")
-                    .textCase(.none)
-            }
-            Section {
-                HStack {
-                    Group {
-                        Image("Chunithm.LogoGlyph.1")
-                            .resizable()
-                        Image("Maimai.LogoGlyph.1")
-                            .resizable()
-                        Image("Ongeki.LogoGlyph.1")
-                            .resizable()
-                        Image("Taiko.LogoGlyph.1")
-                            .resizable()
-                        Image("IIDX.LogoGlyph.1")
-                            .resizable()
-                        Image("SDVX.LogoGlyph.1")
-                            .resizable()
-                        Image("Popn.LogoGlyph.1")
-                            .resizable()
-                        Image("Polaris.LogoGlyph.1")
-                            .resizable()
+                Section {
+                    HStack {
+                        Group {
+                            Image("Chunithm.LogoGlyph.1")
+                                .resizable()
+                            Image("Maimai.LogoGlyph.1")
+                                .resizable()
+                            Image("Ongeki.LogoGlyph.1")
+                                .resizable()
+                            Image("Taiko.LogoGlyph.1")
+                                .resizable()
+                            Image("IIDX.LogoGlyph.1")
+                                .resizable()
+                            Image("SDVX.LogoGlyph.1")
+                                .resizable()
+                            Image("Popn.LogoGlyph.1")
+                                .resizable()
+                            Image("Polaris.LogoGlyph.1")
+                                .resizable()
+                        }
+                        .scaledToFit()
+                        .frame(height: 28.0)
+                        .frame(maxWidth: .infinity)
                     }
-                    .scaledToFit()
-                    .frame(height: 28.0)
-                    .frame(maxWidth: .infinity)
+                } header: {
+                    Text(verbatim: "Logo Icons")
+                        .textCase(.none)
                 }
-            } header: {
-                Text(verbatim: "Logo Icons")
-                    .textCase(.none)
             }
+            .navigationTitle("Template Icons")
         }
     }
 }
