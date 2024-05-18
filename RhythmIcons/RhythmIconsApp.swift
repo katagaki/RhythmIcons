30a54e034dc4278a44545bb751a02e49aaa46fab
katagaki <68231035+katagaki@users.noreply.github.com>
2024-05-18 14:46:45 +0900

    Initial Commit

---
+++ RhythmIcons/RhythmIconsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RhythmIconsApp.swift
+//  RhythmIcons
+//
+//  Created by シン・ジャスティン on 2024/05/18.
+//
+
+import SwiftUI
+
+@main
+struct RhythmIconsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
